fcd6de264dbab1624ea72bb596b6ddf901901452
Matt Wrock <matt@mattwrock.com>
2014-12-03 22:32:36 -0800

    fix resource kit link and add .kitchen directory ti chef ignore

--- recipes/resource_kit.rb
+++ recipes/resource_kit.rb
@@ -1,5 +1,5 @@
 remote_file "#{Chef::Config[:file_cache_path]}\\DSCResourceKit09292014.zip" do
-  source 'http://gallery.technet.microsoft.com/scriptcenter/DSC-Resource-Kit-All-c449312d/file/126120/1/DSC%20Resource%20Kit%20Wave%207%2009292014.zip'
+  source 'https://gallery.technet.microsoft.com/scriptcenter/DSC-Resource-Kit-All-c449312d/file/127765/1/DSC%20Resource%20Kit%20Wave%207%2009292014.zip'
 end
 
 dsc_script 'get-dsc-resource-kit' do
